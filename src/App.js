a1afb40284c1c572843cee9bc0ae41f1a1542a66
Alex Munoz <luismunoz@Luiss-MacBook-Air.local>
2020-01-22 20:32:17 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+  super(props)
+
+  this.state = {
+    iLoggedIn: false
+  }
+  }
+
+  render () {
+  return (
+    <div>Hello</div>
+  );
+}
+}
+
+export default App;
